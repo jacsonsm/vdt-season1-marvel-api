4d35a55187203589c57b9e27343d62f10a493233
Jacson Medeiros <jacsonmedeiros@gmail.com>
2022-06-02 01:14:54 -0300

    Implementaçao do Cadastro e DDELETE

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -1,34 +1,33 @@
-describe('POST/characters', function () {
+describe('POST /characters', function () {
 
   before(function () {
+
+    cy.back2ThePast();
+    cy.setToken()
+
     cy.request({
-      method: 'POST',
-      url: '/sessions',
-      body: {
-        email: 'jacsonmedeiros@gmail.com',
-        password: 'qa-cademy'
-      }
+      method: 'DELETE',
+      url: '/back2thepast/629813936791aa00161c9b5c'
     }).then(function (response) {
-      expect(response.status).to.eql(200)
-      cy.log(response.body.token);
-      Cypress.env('token', response.body.token)
+      expect(response.status).to.eql(200);
+      cy.log(response.body.message)
     })
   })
 
   it('Cadastrar um personagem', function () {
     const character = {
 
-      name: 'Charles Xavier',
-      alias: 'Professor Xavier',
-      team: ['X-Men, Illuminates'],
+      name: 'Wanda Maximoff',
+      alias: 'Feiticeira Escarlate',
+      team: ['Vingadores'],
       active: true
     }
     cy.request({
       method: 'POST',
       url: '/characters',
       body: character,
-      Headers: {
-        Autorization: Cypress.env('token')
+      headers: {
+        Authorization: Cypress.env('token')
       }
     }).then(function (response) {
       expect(response.status).to.eql(201)
@@ -36,3 +35,22 @@ describe('POST/characters', function () {
   });
 })
 
+Cypress.Comands.add('setToken', function () {
+  cy.request({
+    method: 'POST',
+    url: '/sessions',
+    body: {
+      email: 'jacsonmedeiros@gmail.com',
+      password: 'qa-cademy'
+    }
+  }).then(function (response) {
+    expect(response.status).to.eql(200);
+    cy.log(response.body.token);
+    Cypress.env('token', response.body.token);
+
+  })
+})
+
+Cypress.Comands.add('back2Thepast', function () {
+
+})
